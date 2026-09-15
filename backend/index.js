5d5e201ac7970bc13b76438118ddbcf854e7df61
Vastag-Demeter <demetervastag05@gmail.com>
2026-09-15 18:12:57 +0200

    fix: fix cors options

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,6 @@
 import dotenv from "dotenv";
 import app from "./src/app.js";
-import cors from "cors"
 
-const corsOptions = {
-  origin: 'https://market-loop-khaki.vercel.app/',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowHeaders: ['Content-Type', 'Authorization']
-};
-app.use(cors(corsOptions));
 dotenv.config();
 const port = process.env.BACKEND_PORT || process.env.PORT || 4000;
 app.listen(port, '0.0.0.0', () =>
--- backend/src/app.js
+++ backend/src/app.js
@@ -18,11 +18,27 @@ await loadStatuses();
 await loadEmailTypes();
 app.use(express.json());
 app.use(cookieParser());
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://market-loop-khaki.vercel.app',
+]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS policy error: Origin not allowed'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+};
+
+
 app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  }),
+  cors(corsOptions),
 );
 
 app.use("/api", userRouter);
